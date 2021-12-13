582e37e473e5c9e387d95fcbc911050b0680787d
Lyze <11274700+lyze237@users.noreply.github.com>
2021-12-13 22:07:02 +0100

    Fixed html build

--- core/src/main/java/dev/lyze/gamejam/components/MapComponent.java
+++ core/src/main/java/dev/lyze/gamejam/components/MapComponent.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -25,7 +26,7 @@ public class MapComponent extends Component {
         TiledMap map = new TmxMapLoader().load(path);
         var renderer = new OrthogonalTiledMapRenderer(map);
 
-        var layer = map.getLayers().getByType(TiledMapTileLayer.class).get(0);
+        var layer = findTileLayer(map);
 
         var buffer = new FrameBuffer(Pixmap.Format.RGBA8888, layer.getTileWidth() * layer.getWidth(), layer.getTileHeight() * layer.getHeight(), false);
         var cam = new OrthographicCamera();
@@ -42,5 +43,13 @@ public class MapComponent extends Component {
         texture = new TextureRegion(buffer.getColorBufferTexture());
         texture.getTexture().setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
         texture.flip(false, true);
+    }
+
+    private TiledMapTileLayer findTileLayer(TiledMap map) {
+        for (MapLayer mapLayer : map.getLayers())
+            if (mapLayer instanceof TiledMapTileLayer)
+                return ((TiledMapTileLayer) mapLayer);
+
+        return null;
     }
 }
